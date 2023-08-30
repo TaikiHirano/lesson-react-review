018ad2ffcb68073c46e9af34a390f8f3b7f37196
hirano <rhi691508@gmail.com>
2023-08-31 03:38:58 +0900

    feat: Creating a button that changes text when pressed

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,13 @@ import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [status, setStatus] = useState<string>("text");
+
   return (
     <div className="App">
       <header className="App-header">
+        <h4>{status}</h4>
+        <button onClick={() => setStatus("new text")}>Button</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
